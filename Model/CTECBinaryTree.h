aa8b2694163e88490d884715943bc284d2cf6dbc
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-04-21 08:47:50 -0600

    Added //done

--- Model/CTECBinaryTree.h
+++ Model/CTECBinaryTree.h
@@ -33,7 +33,7 @@ public:
     int getHeight();
     int getSize();  //Done
     bool isBalanced();
-    TreeNode<Type> * getRoot();
+    TreeNode<Type> * getRoot(); //Done
     void inorderTraversal(TreeNode<Type> * currentNode);    //Done
     void preorderTraversal(TreeNode<Type> * currentNode);   //Done
     void postorderTraversal(TreeNode<Type> * currentNode);  //Done
